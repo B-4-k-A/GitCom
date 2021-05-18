f471e07baa2dffeddf94a630de946effe5921278
Sharifov Sardorbek <sardorbek-sharifov@mail.ru>
2021-05-19 02:04:48 +0500

    add parser and delete comments functionality

--- out/extension.js
+++ out/extension.js
@@ -1,27 +1,30 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const parser_1 = require("./parser");
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gitcom" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('gitcom.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from GitCom!');
+    let activeEditor;
+    let parser = new parser_1.Parser();
+    let removeComments = function (n) {
+        if (!activeEditor || !parser.supportedLanguage) {
+            return;
+        }
+        parser.FindSingleLineComments(activeEditor);
+        parser.FindMultilineComments(activeEditor);
+        vscode.workspace.applyEdit(parser.edit);
+    };
+    // Register commands here
+    let removeAllCommentsCommand = vscode.commands.registerCommand('gitcom.removeAllComments', () => {
+        if (vscode.window.activeTextEditor) {
+            activeEditor = vscode.window.activeTextEditor;
+            parser.SetRegex(activeEditor, activeEditor.document.languageId);
+            removeComments(2);
+        }
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(removeAllCommentsCommand);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
